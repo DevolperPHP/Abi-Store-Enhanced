e0843ba9b5389c8c83a5c41fd37959442cd96149
Mohammed Majid <mohammedmajid@Mohammeds-MacBook-Air.local>
2025-12-11 23:18:20 +0300

    Transfer page

--- routes/admin/transfer.js
+++ routes/admin/transfer.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 router.use(isAdminMiddleWare)
 router.use(isUserMiddleWare)
 
+// Transfer from Main Storage to Local Store
 router.get('/', async (req, res) => {
     try {
         const productsItems = await Product.find({})
@@ -45,6 +46,7 @@ router.get('/', async (req, res) => {
                 products: formattedProducts,
                 data: uniqueArray,
                 err: req.flash('err'),
+                direction: 'to-store'
             })
 
         } else {
@@ -53,6 +55,61 @@ router.get('/', async (req, res) => {
                 products: [],
                 data: uniqueArray,
                 err: req.flash('err'),
+                direction: 'to-store'
+            })
+        }
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+// Transfer from Local Store to Main Storage
+router.get('/to-main', async (req, res) => {
+    try {
+        const productsItems = await Product.find({})
+        const data = productsItems.map((item) => `${item.name} - ${item.size}`)
+        const uniqueArray = [... new Set(data)]
+
+        const transferItems = req.user.transfer;
+        if (transferItems.length > 0) {
+
+            const qtyMap = req.user.transfer.reduce((map, item) => {
+                map[item.id.toString()] = { qty: Number(item.qty) || 0, addedAt: item.addedAt };
+                return map;
+            }, {});
+
+            const products = await Product.find({ _id: { $in: transferItems.map(item => item.id) } });
+            const formattedProducts = products.map(product => {
+                const productIdStr = product._id.toString();
+                const qtyData = qtyMap[productIdStr];
+                return {
+                    _id: product._id,
+                    name: product.name,
+                    price: product.price,
+                    image: product.image,
+                    colorName: product.colorName,
+                    size: product.size,
+                    qty: Number(qtyData?.qty) || 0,
+                    addedAt: qtyData?.addedAt || Date.now()
+                };
+            }).sort((a, b) => b.addedAt - a.addedAt);
+
+
+            res.render('transfer/index', {
+                user: req.user,
+                products: formattedProducts,
+                data: uniqueArray,
+                err: req.flash('err'),
+                direction: 'to-main'  // Pass direction flag
+            })
+
+        } else {
+            res.render('transfer/index', {
+                user: req.user,
+                products: [],
+                data: uniqueArray,
+                err: req.flash('err'),
+                direction: 'to-main'  // Pass direction flag
             })
         }
     } catch (err) {
@@ -64,6 +121,7 @@ router.put('/add-by-code/:code', async (req, res) => {
     try {
         const data = await Product.findOne({ barcode: req.params.code })
         const products = req.user.transfer
+        const direction = req.query.direction || 'to-store'
         if (data) {
             const filter = products.find(item => item.id == data.id)
             if (filter) {
@@ -87,7 +145,13 @@ router.put('/add-by-code/:code', async (req, res) => {
         } else {
             req.flash('err', 'Product not found')
         }
-        res.redirect('/transfer')
+
+        // Redirect to the correct transfer page based on direction
+        if (direction === 'to-main') {
+            res.redirect('/transfer/to-main')
+        } else {
+            res.redirect('/transfer')
+        }
     } catch (err) {
         console.log(err);
     }
@@ -145,7 +209,15 @@ router.put('/confirm', async (req, res) => {
         const transferItems = req.user.transfer
         const ids = transferItems.map(item => mongoose.Types.ObjectId(item.id))
         const products = await Product.find({ _id: { $in: ids } })
+
+        if (add == min) {
+            req.flash('err', "You can't transfer items to the same storage")
+            res.redirect('/transfer')
+            return
+        }
+
         if (min == "Main Storage" && add == "Local Store") {
+            // Transfer FROM Main Storage TO Local Store
             for (let product of products) {
                 const item = transferItems.find(item => item.id == product._id.toString())
                 if (item) {
@@ -154,17 +226,15 @@ router.put('/confirm', async (req, res) => {
                     await product.save();
                 }
             }
-
             req.user.transfer = []
             await req.user.save()
-        }
-
-        if (add == "Main Storage" && min == "Local Store") {
+        } else if (min == "Local Store" && add == "Main Storage") {
+            // Transfer FROM Local Store TO Main Storage
             for (let product of products) {
                 const item = transferItems.find(item => item.id == product._id.toString())
                 if (item) {
-                    product.qty += Number(item.qty)
                     product.stock -= Number(item.qty)
+                    product.qty += Number(item.qty)
                     await product.save();
                 }
             }
@@ -172,9 +242,6 @@ router.put('/confirm', async (req, res) => {
             await req.user.save()
         }
 
-        if (add == min) {
-            req.flash('err', "You can't transfer items to the same storage")
-        }
         res.redirect('/transfer')
     } catch (err) {
         console.log(err);
@@ -186,6 +253,7 @@ router.get('/search/:name', async (req, res) => {
     try {
         const searchValue = req.params.name;
         const [name, size] = searchValue.split(' - ');
+        const direction = req.query.direction || 'to-store';  // Get direction from query param
 
         const products = await Product.find({ name, size });
 
@@ -193,7 +261,8 @@ router.get('/search/:name', async (req, res) => {
 
         res.render('transfer/search', {
             user: req.user,
-            products: products
+            products: products,
+            direction: direction
         });
 
     } catch (err) {
@@ -206,12 +275,14 @@ router.get('/search/get-qty/:id', async (req, res) => {
     try {
         const id = req.params.id;
         const data = await Product.findOne({ _id: id });
+        const direction = req.query.direction || 'to-store';
 
         if (!req.user) return res.redirect("/passport/sign-up");
 
         res.render('transfer/search-qty', {
             user: req.user,
-            data
+            data: data,
+            direction: direction
         });
 
     } catch (err) {
@@ -224,7 +295,7 @@ router.put('/add/:id', async (req, res) => {
   try {
     const userId = req.user.id;
     const productId = mongoose.Types.ObjectId(req.params.id); // 🔧 ensure ObjectId
-    const { qty } = req.body;
+    const { qty, direction } = req.body;
 
     const [product, user] = await Promise.all([
       Product.findOne({ _id: productId }),
@@ -257,7 +328,12 @@ router.put('/add/:id', async (req, res) => {
       );
     }
 
-    res.redirect('/transfer');
+    // Redirect back to the correct transfer page based on direction
+    if (direction === 'to-main') {
+      res.redirect('/transfer/to-main');
+    } else {
+      res.redirect('/transfer');
+    }
 
   } catch (err) {
     console.error('Error adding to transfer:', err);
